85b290b9a3cd8d9cf6ea7348429a7a60b777d2e1
Mohd Aqib <37264147+Mohdaqib18@users.noreply.github.com>
2023-12-07 17:11:37 +0530

    adjusted navbar styling

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,19 +5,25 @@ import React from 'react'
 const Navbar = () => {
   return (
 		<nav className="bg-[#00A] flex justify-between items-center px-4 py-4 rounded-md">
-			<Link href={"/"}>
-				<Image
-					src="/logo.png"
-					width={25}
-					height={25}
-					alt="Picture of the author"
-					className="border border-white rounded-r-sm"
-				/>
-			</Link>
-			<Link href={"/"} className="font-bold text-white ml-[-730px]">
-				Tech Space
-			</Link>
-			<Link href={"/addPost"} className="bg-white p-2 font-semibold text-[14px] rounded-md">
+			<div className="flex justify-between w-[130px] ">
+				<Link href={"/"}>
+					<Image
+						src="/logo.png"
+						width={25}
+						height={25}
+						alt="Picture of the author"
+						className="border border-white rounded-r-sm "
+					/>
+				</Link>
+
+				<Link href={"/"} className="font-bold text-white ">
+					Tech Space
+				</Link>
+			</div>
+			<Link
+				href={"/addPost"}
+				className="bg-white p-2 font-semibold text-[14px] rounded-md"
+			>
 				Add Post
 			</Link>
 		</nav>
